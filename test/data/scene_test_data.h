428c213f9840a1c3cbec5131dea1caf48a88d89e
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2023-12-29 00:11:04 +0000

    test data for testing Scene class

---
+++ test/data/scene_test_data.h
@@ -0,0 +1,19 @@
+static const char* valid_background_scene =
+"................................................................\n"
+"|                                                              |\n"
+"|                 TTTTT  EEEE    SSS  TTTTT                    |\n"
+"|                   T    E      S       T                      |\n"
+"|                   T    EEEE    SS     T                      |\n"
+"|                   T    E         S    T                      |\n"
+"|                   T    EEEE   SSS     T                      |\n"
+"................................................................\n";
+
+static const char* invalid_background_scene = 
+".....\n"
+".........\n"
+"....\n";
+
+static int expected_scene_width = 64;
+static int expected_scene_height = 8;
+
+static const char* scene_object = "@";
