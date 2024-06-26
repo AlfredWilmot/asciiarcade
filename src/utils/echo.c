ad0ea65e21378ec5a10f750786e47711d8ce8c71
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2024-06-26 23:51:31 +0100

    some info

--- src/utils/echo.c
+++ src/utils/echo.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-
+/*Simply print input params as a single column; a noddy echo*/
 int main(int argc, char **argv)
 {
-  char ch;
   for (int i=1; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
